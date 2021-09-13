9a120288dc5d178f5a76a015aaf3b109ce5b4230
saddamarbaa <saddamarbaasaddamarbaa@gmail.com>
2021-09-13 21:14:15 +0700

    feat: add hir us page

---
+++ pages/hir-us.js
@@ -0,0 +1,23 @@
+/** @format */
+
+import { Fragment } from "react";
+import Head from "next/head";
+
+import HirUsPageComponent from "../components/hire-us-page/hir-us";
+function HirUsPage(props) {
+	return (
+		<Fragment>
+			<Head>
+				<title>Hire Us</title>
+				<meta
+					name='description'
+					content='Let’s get to work! Please answer a couple of short questions about you and your project'
+				/>
+			</Head>
+
+			<HirUsPageComponent />
+		</Fragment>
+	);
+}
+
+export default HirUsPage;
